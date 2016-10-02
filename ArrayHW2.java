31a70a0c7fa623d0451cb4e35109a5cc81894509
Pinchen Huang <wulala505@gmail.com>
2016-10-02 22:56:01 +0800

    Update ArrayHW2.java

--- ArrayHW2.java
+++ ArrayHW2.java
@@ -5,21 +5,21 @@ public class ArrayHW2 {
 	int arr[] = new int[42];
 	int index[] = new int[42];
 
-	void ranArr() {
+	public void ranArr() {
 		for (int i = 1; i <= 100000; i++) {
 			int j = (int) (Math.random() * 42);
 			arr[j]++;
 		}
 	}
 
-	void Index() {
+	public void Index() {
 		for (int i = 0; i < 42; i++) {
 			index[i] = i + 1;
 		}
 	}
 
 	// 起泡排序
-	void Sort() {
+	public void Sort() {
 		for (int x = 0; x < arr.length; x++) {
 			for (int y = x + 1; y < arr.length; y++) {
 				if (arr[x] > arr[y]) {
